e7eed4f29b25346cf3bd4c5dd7c8bfa7c81545e3
Matas Pigaga <mataspigaga@gmail.com>
2021-07-11 13:34:57 -0400

    added Link element to maintain SPA functionality

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
 // domain.com/news
+import Link from "next/link";
 
 const NewsPage = () => {
-  return <h1>The News Page!</h1>;
+  return (
+    <>
+      <h1>The News Page!</h1>
+      <ul>
+        <li>
+          <Link href="/news/something">Something</Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
